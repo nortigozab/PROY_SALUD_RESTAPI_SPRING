fc04670698527b3812d25590da219c2a4fc5aeb2
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-07-13 07:53:04 -0500

    feat: Inizializacion de proyecto

---
+++ src/main/java/com/api/PROY_SALUD_RESTAPI_SPRING/ProySaludRestapiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.api.PROY_SALUD_RESTAPI_SPRING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProySaludRestapiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProySaludRestapiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/PROY_SALUD_RESTAPI_SPRING/ProySaludRestapiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.PROY_SALUD_RESTAPI_SPRING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProySaludRestapiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
