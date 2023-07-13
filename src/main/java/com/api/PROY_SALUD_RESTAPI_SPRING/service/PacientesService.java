157d2a025bc0ff2e391c7db365ee5880d3ed258a
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-07-13 10:18:23 -0500

    feat(Paciente): servicio de paciente

---
+++ src/main/java/com/api/PROY_SALUD_RESTAPI_SPRING/service/PacientesService.java
@@ -0,0 +1,29 @@
+package com.api.PROY_SALUD_RESTAPI_SPRING.service;
+
+import com.api.PROY_SALUD_RESTAPI_SPRING.entity.Pacientes;
+import com.api.PROY_SALUD_RESTAPI_SPRING.repository.PacientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PacientesService {
+    @Autowired
+    PacientesRepository pacientesRepository;
+    public List<Pacientes> getPacientes(){
+        return pacientesRepository.findAll();
+    }
+    public Optional<Pacientes> getPaciente(Long id_paciente){
+        return pacientesRepository.findById(id_paciente);
+    }
+
+    public Pacientes saveOrUpdate(Pacientes pacientes){
+        return pacientesRepository.save(pacientes);
+    }
+
+    public void delete(Long id_paciente){
+        pacientesRepository.deleteById(id_paciente);
+    }
+}
