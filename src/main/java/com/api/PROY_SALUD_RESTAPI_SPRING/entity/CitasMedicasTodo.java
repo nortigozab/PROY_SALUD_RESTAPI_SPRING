8f7c4623c7b027ac3cac9d357d9215b6d9274c74
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-07-14 00:19:14 -0500

    feat(CitasMedicas): Se crea modelo de CitasMedicas con detalle de paciente, doctor y especialidad

---
+++ src/main/java/com/api/PROY_SALUD_RESTAPI_SPRING/entity/CitasMedicasTodo.java
@@ -0,0 +1,27 @@
+package com.api.PROY_SALUD_RESTAPI_SPRING.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "citas_medicas")
+public class CitasMedicasTodo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_cita;
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_doctor")
+    private DoctoresEspecialidad doctor;
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_paciente")
+    private Pacientes paciente;
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "especialidad")
+    private Especialidades especialidad;
+    private Date fecha;
+    private boolean disponibilidad;
+}
