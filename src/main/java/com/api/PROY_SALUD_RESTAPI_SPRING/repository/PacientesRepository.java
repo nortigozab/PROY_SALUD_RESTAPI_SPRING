81f6586e5706aec60ce58a07994a5a2e36c35a12
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-07-14 00:16:29 -0500

    feat(Paciente): Se Ajusta Repository de paciente para buscar por numero_cedula

--- src/main/java/com/api/PROY_SALUD_RESTAPI_SPRING/repository/PacientesRepository.java
+++ src/main/java/com/api/PROY_SALUD_RESTAPI_SPRING/repository/PacientesRepository.java
@@ -1,9 +1,15 @@
 package com.api.PROY_SALUD_RESTAPI_SPRING.repository;
 
+import com.api.PROY_SALUD_RESTAPI_SPRING.entity.Doctores;
 import com.api.PROY_SALUD_RESTAPI_SPRING.entity.Pacientes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PacientesRepository  extends JpaRepository<Pacientes,Long> {
+    @Query("SELECT p FROM Pacientes p WHERE p.numero_cedula = ?1")
+    List<Pacientes> findByCedula(String numero_cedula);
 }
