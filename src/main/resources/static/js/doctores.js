746e1ffa6e50bd44bfd163b9a8f7a02dc927b46f
nortigozab <77377179+nortigozab@users.noreply.github.com>
2023-07-17 23:53:56 -0500

    style(doctores): Se ajusta legibilidad del codigo singlepage

--- src/main/resources/static/js/doctores.js
+++ src/main/resources/static/js/doctores.js
@@ -1,38 +1,28 @@
 crearForm.addEventListener("submit", (event) => {
-    event.preventDefault(); // Evitar que el formulario se envíe por defecto
-
-    // Obtener los valores de los campos del formulario
+    event.preventDefault();
     const nombre = document.getElementById("nombre").value;
     const apellido = document.getElementById("apellido").value;
     const consultorio = document.getElementById("consultorio").value;
     const especialidad = document.getElementById("especialidad").value;
     const correo = document.getElementById("correo").value;
-
-    // Validar que ningún campo esté vacío
     if (!nombre || !apellido || !consultorio || !especialidad || !correo) {
         window.alert("Por favor, complete todos los campos.");
-        return; // Detener la ejecución del código si algún campo está vacío
+        return;
     }
-
-    // Expresión regular para validar que el campo "consultorio" contenga solo números
     const numberRegex = /^[0-9]+$/;
 
-    // Validar que el campo "consultorio" contenga solo números
     if (!numberRegex.test(consultorio)) {
         window.alert("El campo 'consultorio' debe contener solo números.");
-        return; // Detener la ejecución del código si el campo "consultorio" no contiene solo números
+        return;
     }
-
-    // Expresión regular para validar el formato de correo electrónico
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // Validar que el correo tenga un formato válido
     if (!emailRegex.test(correo)) {
         window.alert("Por favor, ingrese un correo electrónico válido.");
-        return; // Detener la ejecución del código si el correo no tiene un formato válido
+        return;
     }
 
-    // Crear el objeto de doctor con los datos del formulario
+
     const doctor = {
         nombre: nombre,
         apellido: apellido,
@@ -40,8 +30,6 @@ crearForm.addEventListener("submit", (event) => {
         especialidad: especialidad,
         correo_contacto: correo
     };
-
-    // Realizar una solicitud POST para guardar el doctor
     fetch("/api/doctores", {
         method: "POST",
         headers: {
@@ -51,23 +39,17 @@ crearForm.addEventListener("submit", (event) => {
     })
         .then(response => response.json())
         .then(nuevoDoctor => {
-            // Limpiar el formulario y actualizar la lista de doctores
             crearForm.reset();
             getDoctores();
         })
         .catch(error => console.log("Error al crear el doctor:", error));
 });
-
-// Función para obtener la lista de doctores
 function getDoctores() {
     fetch("/api/doctores")
         .then(response => response.json())
         .then(data => {
-            // Limpiar la tabla de doctores
             const doctoresTable = document.getElementById("doctoresTable");
             doctoresTable.innerHTML = "";
-
-            // Llenar el select de especialidades
             const especialidadSelect = document.getElementById("especialidad");
             especialidadSelect.innerHTML = "";
             const defaultOption = document.createElement("option");
@@ -80,8 +62,6 @@ function getDoctores() {
                 option.textContent = especialidad.nombre_especialidad;
                 especialidadSelect.appendChild(option);
             });
-
-            // Iterar sobre los doctores y agregarlos a la tabla
             data.doctores.forEach(doctor => {
                 const row = document.createElement("tr");
                 row.innerHTML = `
@@ -101,19 +81,15 @@ function getDoctores() {
         })
         .catch(error => console.log("Error al obtener los doctores:", error));
 }
-// Función para eliminar un doctor
 function eliminarDoctor(id_doctor) {
     if (confirm(`¿Estás seguro de eliminar este al doctor ${id_doctor}?`)) {
         fetch(`/api/doctores/${id_doctor}`, {
             method: "DELETE"
         })
             .then(response => {
-                // Actualizar la lista de doctores
                 getDoctores();
             })
             .catch(error => console.log("Error al eliminar el doctor:", error));
     }
 }
-
-// Cargar la lista de doctores al cargar la página
 window.addEventListener("DOMContentLoaded", getDoctores);
